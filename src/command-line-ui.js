825990d8929f8c57d5dbf39f38303da1a7e8887f
Nemanja <nemanja.vasic@codecentric.de>
2019-10-05 11:24:45 +0200

    Delete visualizer

--- src/command-line-ui.js
+++ src/command-line-ui.js
@@ -1,5 +1,5 @@
-import React, {Component} from 'react';
-import {Box, render, Static, Text} from 'ink';
+import React, { Component } from 'react';
+import { Box, render, Static, Text } from 'ink';
 import Divider from 'ink-divider';
 import SelectInput from 'ink-select-input';
 import chalk from "chalk";
@@ -9,60 +9,57 @@ import open from "open";
 import cliItems from './json-files/cli-items';
 import introText from './json-files/intro-text';
 
-const memoryVisualizer = require('memory-visualizer');
-
 const log = console.log;
 
 class CommandLineUi extends Component {
 
-	constructor() {
-		super();
-		this.state = {cliItems: cliItems}
-	}
+  constructor() {
+    super();
+    this.state = { cliItems: cliItems }
+  }
 
-	createItems = items => {
-		for (const item of items) {
-			item.key = item.url || item.label;
-		}
+  createItems = items => {
+    for (const item of items) {
+      item.key = item.url || item.label;
+    }
 
-		return items;
-	};
+    return items;
+  };
 
-	handleSelect = cliItem => {
-		if (cliItem.url) {
-			open(cliItem.url);
-		}
-		if (cliItem.action === 'exit') {
-			process.exit();
-		}
-	};
+  handleSelect = cliItem => {
+    if (cliItem.url) {
+      open(cliItem.url);
+    }
+    if (cliItem.action === 'exit') {
+      process.exit();
+    }
+  };
 
-	showBlogs = item => {
-		if (item.label === 'Blogs') {
-			item.blogs.forEach(blog => log(chalk.green(blog.name, chalk.underline.bgBlue(blog.url))));
-		}
-	};
+  showBlogs = item => {
+    if (item.label === 'Blogs') {
+      item.blogs.forEach(blog => log(chalk.green(blog.name, chalk.underline.bgBlue(blog.url))));
+    }
+  };
 
-	render() {
-		memoryVisualizer.start(60, 'heapTotal', 1200);
-		return (
-			<Box flexDirection="column" marginBottom={1}>
-				<Box marginBottom={1}>
-					{log(chalk.red(figlet.textSync('NeMaNJaS CLI', {horizontalLayout: 'full'})))}
-				</Box>
-				<Box marginBottom={1}>
-					<Text>{chalk.bold(introText.intro)}</Text>
-				</Box>
-				<Divider title={'You can find me on'} titleColor={'red'} dividerColor={'blue'}/>
-				<Box>
-					<SelectInput
-						items={this.createItems(this.state.cliItems)}
-						onSelect={this.handleSelect}
-						onHighlight={item => this.showBlogs(item)}/>
-				</Box>
-			</Box>
-		);
-	}
+  render() {
+    return (
+      <Box flexDirection="column" marginBottom={1}>
+        <Box marginBottom={1}>
+          {log(chalk.red(figlet.textSync('NEMANJAS CLI', { horizontalLayout: 'full' })))}
+        </Box>
+        <Box marginBottom={1}>
+          <Text>{chalk.bold(introText.intro)}</Text>
+        </Box>
+        <Divider title={'You can find me on'} titleColor={'red'} dividerColor={'blue'} />
+        <Box>
+          <SelectInput
+            items={this.createItems(this.state.cliItems)}
+            onSelect={this.handleSelect}
+            onHighlight={item => this.showBlogs(item)} />
+        </Box>
+      </Box>
+    );
+  }
 }
 
-render(<CommandLineUi/>);
+render(<CommandLineUi />);
